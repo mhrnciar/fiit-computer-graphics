c8c75101fab6575d12f0b0ad3aefbb874ae9863c
Matej Hrnciar <matej.hrnciar2@gmail.com>
2021-10-14 17:30:34 +0200

    Finish task3

--- src/task3_bezier/task3_bezier.cpp
+++ src/task3_bezier/task3_bezier.cpp
@@ -1,4 +1,4 @@
-// Task 4 - Render the letter R using OpenGL
+// Task 3 - Render the letter R using OpenGL
 //        - Implement a function to generate bezier curve points
 //        - Generate multiple points on each bezier curve
 //        - Draw lines connecting the generated points using OpenGL
@@ -16,111 +16,164 @@ const unsigned int SIZE = 512;
 class BezierWindow : public ppgso::Window {
 private:
 
-  // Control points for the bezier curves
-  // First curve is 4 control points
-  // Rest of the curves are 3 control points, each reusing the last curve end
-  // Defines the letter "R"
-  std::vector<glm::vec2> controlPoints = {
-      { 0,  -1},
-      { 0, -.3},
-      { 0,  .3},
-      { 0,   1},
-      {.3,   1},
-      {.5,   1},
-      {.5,  .5},
-      {.5,   0},
-      {.3,   0},
-      { 0,   0},
-      {.3, -.3},
-      {.5, -.5},
-      {.5,  -1},
-  };
+    // Control points for the bezier curves
+    // First curve is 4 control points
+    // Rest of the curves are 3 control points, each reusing the last curve end
+    // Defines mushroom shape
+    std::vector<glm::vec2> controlPoints = {
+            { 0.636,  0.22},
+            { 0.465, 0.94},
+            { -0.465,  0.94},
+            { -0.636,   0.22},
+            {0.32,   0.22},
+            {-0.32,   0.22},
+            { 0.636,  0.22},
+    };
 
-  // This will hold the bezier curve geometry once we generate it
-  std::vector<glm::vec3> points;
+    std::vector<glm::vec2> controlPoints2 = {
+            {0.13, 0.22},
+            {0.385, -0.95},
+            {-0.385, -0.95},
+            {-0.13, 0.22}
+    };
 
-  // GLSL Program to use for rendering
-  ppgso::Shader program = {color_vert_glsl, color_frag_glsl};
+    /* Defines the letter "R"
+    std::vector<glm::vec2> controlPoints = {
+            { 0,  -1},
+            { 0, -.3},
+            { 0,  .3},
+            { 0,   1},
+            {.3,   1},
+            {.5,   1},
+            {.5,  .5},
+            {.5,   0},
+            {.3,   0},
+            { 0,   0},
+            {.3, -.3},
+            {.5, -.5},
+            {.5,  -1},
+    };
+     */
 
-  // These numbers are used to pass buffer data to OpenGL
-  GLuint vao = 0, vbo = 0;
+    // This will hold the bezier curve geometry once we generate it
+    std::vector<glm::vec3> points;
 
-  // Compute points for Bezier curve using 4 control points
-  glm::vec2 bezierPoint(const glm::vec2 &p0, const glm::vec2 &p1, const glm::vec2 &p2, const glm::vec2 &p3, const float t) {
-    // TODO: Compute point on the Bezier curve
-    return {};
-  }
+    // GLSL Program to use for rendering
+    ppgso::Shader program = {color_vert_glsl, color_frag_glsl};
 
-  // Compute points for a sequence of Bezier curves defined by a vector of control points
-  // Each bezier curve will reuse the end point of the previous curve
-  // count - Number of points to generate on each curve
-  void bezierShape(int count) {
-    for(int i = 1; i < (int) controlPoints.size(); i+=3) {
-      for (int j = 0; j <= count; j++) {
-        // TODO: Generate points for each Bezier curve and insert them
-        glm::vec2 point; //= ??
-        points.emplace_back(point, 0);
-      }
+    // These numbers are used to pass buffer data to OpenGL
+    GLuint vao = 0, vbo = 0;
+
+    glm::vec2 interpolate(const glm::vec2 &p0, const glm::vec2 &p1, const float t){
+        float x = (1-t) * p0.x + t * p1.x;
+        float y = (1-t) * p0.y + t * p1.y;
+        return {x, y};
     }
-  }
+
+    // Compute points for Bezier curve using 4 control points
+    glm::vec2 bezierPoint(const glm::vec2 &p0, const glm::vec2 &p1, const glm::vec2 &p2, const glm::vec2 &p3, const float t) {
+        // TODO: Compute point on the Bezier curve
+        // De Casteljau Algorithm
+        glm::vec2 linear_p1 = interpolate(p0, p1, t);
+        glm::vec2 linear_p2 = interpolate(p1, p2, t);
+        glm::vec2 linear_p3 = interpolate(p2, p3, t);
+
+        glm::vec2 quadratic_p1 = interpolate(linear_p1, linear_p2, t);
+        glm::vec2 quadratic_p2 = interpolate(linear_p2, linear_p3, t);
+
+        glm::vec2 cubic_p1 = interpolate(quadratic_p1, quadratic_p2, t);
+        return cubic_p1;
+
+        /* Calculation by equation
+        float cubic_bezier_x = (powf((1-t),3) * p0.x) + (3 * powf((1-t), 2) * t * p1.x) + (3 * (1 - t) * powf(t,2) * p2.x) + (powf(t,3) * p3.x);
+        float cubic_bezier_y = (powf((1-t),3) * p0.y) + (3 * powf((1-t), 2) * t * p1.y) + (3 * (1 - t) * powf(t,2) * p2.y) + (powf(t,3) * p3.y);
+
+        return {cubic_bezier_x, cubic_bezier_y};
+         */
+    }
+
+    // Compute points for a sequence of Bezier curves defined by a vector of control points
+    // Each bezier curve will reuse the end point of the previous curve
+    // count - Number of points to generate on each curve
+    void bezierShape(int count) {
+        for(int i = 1; i < (int) controlPoints.size(); i+=3) {
+            for (int j = 0; j <= count; j++) {
+                // TODO: Generate points for each Bezier curve and insert them
+                glm::vec2 point;
+                float t = 1 / float(count) * float(j);
+                point = bezierPoint(controlPoints[i - 1], controlPoints[i], controlPoints[i + 1], controlPoints[i + 2], t);
+                points.emplace_back(point, 0);
+            }
+        }
+        // For the foot of mushroom
+        for(int i = 1; i < (int) controlPoints2.size(); i+=3) {
+            for (int j = 0; j <= count; j++) {
+                // TODO: Generate points for each Bezier curve and insert them
+                glm::vec2 point; //= ??
+                float t = 1 / float(count) * float(j);
+                point = bezierPoint(controlPoints2[i - 1], controlPoints2[i], controlPoints2[i + 1], controlPoints2[i + 2], t);
+                points.emplace_back(point, 0);
+            }
+        }
+    }
 
 public:
-  BezierWindow() : Window{"task3_bezier", SIZE, SIZE} {
-    // Generate Bezier curve points
-    bezierShape(15);
+    BezierWindow() : Window{"task3_bezier", SIZE, SIZE} {
+        // Generate Bezier curve points
+        bezierShape(15);
 
-    // Generate a vertex array object
-    // This keeps track of what attributes are associated with buffers
-    glGenVertexArrays(1, &vao);
-    glBindVertexArray(vao);
+        // Generate a vertex array object
+        // This keeps track of what attributes are associated with buffers
+        glGenVertexArrays(1, &vao);
+        glBindVertexArray(vao);
 
-    // Generate a vertex buffer object, this will feed data to the vertex shader
-    glGenBuffers(1, &vbo);
-    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+        // Generate a vertex buffer object, this will feed data to the vertex shader
+        glGenBuffers(1, &vbo);
+        glBindBuffer(GL_ARRAY_BUFFER, vbo);
 
-    // TODO: Pass the control points to the GPU
-    // glBufferData(GL_ARRAY_BUFFER, ???, ???, GL_STATIC_DRAW);
+        // TODO: Pass the control points to the GPU
+        glBufferData(GL_ARRAY_BUFFER, points.size() * sizeof(glm::vec3), points.data(), GL_STATIC_DRAW);
 
-    // Setup vertex array lookup, this tells the shader how to pick data for the "Position" input
-    auto position_attrib = program.getAttribLocation("Position");
-    glVertexAttribPointer(position_attrib, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
-    glEnableVertexAttribArray(position_attrib);
+        // Setup vertex array lookup, this tells the shader how to pick data for the "Position" input
+        auto position_attrib = program.getAttribLocation("Position");
+        glVertexAttribPointer(position_attrib, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+        glEnableVertexAttribArray(position_attrib);
 
-    // Set model matrix to identity
-    program.setUniform("ModelMatrix", glm::mat4{});
-    program.setUniform("ViewMatrix", glm::mat4{});
-    program.setUniform("ProjectionMatrix", glm::mat4{});
+        // Set model matrix to identity
+        program.setUniform("ModelMatrix", glm::mat4{1.0});
+        program.setUniform("ViewMatrix", glm::mat4{1.0});
+        program.setUniform("ProjectionMatrix", glm::mat4{1.0});
 
-    // Set the color uniform
-    program.setUniform("OverallColor", glm::vec3{1.0f,1.0f,1.0f});
-  }
+        // Set the color uniform
+        program.setUniform("OverallColor", glm::vec3{1.0f,1.0f,1.0f});
+    }
 
-  ~BezierWindow() final {
-    glDeleteBuffers(1, &vbo);
-    glDeleteVertexArrays(1, &vao);
-  }
+    ~BezierWindow() final {
+        glDeleteBuffers(1, &vbo);
+        glDeleteVertexArrays(1, &vao);
+    }
 
-  void onIdle() final {
-    // Set gray background
-    glClearColor(0.5f,0.5f,0.5f,0);
+    void onIdle() final {
+        // Set gray background
+        glClearColor(0.5f,0.5f,0.5f,0);
 
-    // Clear depth and color buffers
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        // Clear depth and color buffers
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    // Draw shape
-    glBindVertexArray(vao);
+        // Draw shape
+        glBindVertexArray(vao);
 
-    // TODO: Define the correct render mode
-    //glDrawArrays(??, 0, ??);
-  }
+        // TODO: Define the correct render mode
+        glDrawArrays(GL_LINE_STRIP, 0, (GLsizei) points.size());
+    }
 };
 
 int main() {
-  // Create our window
-  BezierWindow window;
+    // Create our window
+    BezierWindow window;
 
-  // Main execution loop
-  while (window.pollEvents()) {}
+    // Main execution loop
+    while (window.pollEvents()) {}
 
-  return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
