3514a69ac9a70308801ed99f8230dca128e5e367
Matej Hrnciar <matej.hrnciar2@gmail.com>
2021-10-14 17:31:14 +0200

    Finish task4

--- src/task4_2dshapes/task4_2dshapes.cpp
+++ src/task4_2dshapes/task4_2dshapes.cpp
@@ -1,4 +1,4 @@
-// Task 5 - Draw a 2D shape using polygons and animate it
+// Task 4 - Draw a 2D shape using polygons and animate it
 //        - Encapsulate the shape using a class
 //        - Use color_vert/frag shader to display the polygon
 //        - Animate the object position, rotation and scale.
@@ -20,129 +20,151 @@ const unsigned int SIZE = 512;
 // Object to represent 2D OpenGL shape
 class Shape {
 private:
-  // 2D vectors define points/vertices of the shape
-  // TODO: Define your shape points
-  std::vector<glm::vec3> vetrices;
+    // 2D vectors define points/vertices of the shape
+    // TODO: Define your shape points
+    std::vector<glm::vec3> vetrices {
+            {-0.2f, 0.2f, 0.0f},
+            {0.2f, 0.2f, 0.0f},
+            {-0.2f, -0.2f, 0.0f},
+            {0.2f, -0.2f, 0.0f},
 
-  // Structure representing a triangular face, usually indexes into vertices
-  struct Face {
-    // TODO: Define your face structure
-  };
+            {0.0f, 1.0f, 0.0f},
+            {1.0f, 0.0f, 0.0f},
+            {0.0f, -1.0f, 0.0f},
+            {-1.0f, 0.0f, 0.0f},
 
-  // Indices define triangles that index into vertices
-  // TODO: Define your mesh indices
-  std::vector<Face> mesh;
+    };
 
-  // Program to associate with the object
-  ppgso::Shader program = {color_vert_glsl, color_frag_glsl};
+    // Structure representing a triangular face, usually indexes into vertices
+    struct Face {
+        // TODO: Define your face structure
+        GLuint v0, v1, v2;
+    };
 
-  // These will hold the data and object buffers
-  GLuint vao, vbo, cbo, ibo;
-  glm::mat4 modelMatrix{1.0f};
+    // Indices define triangles that index into vertices
+    // TODO: Define your mesh indices
+    std::vector<Face> mesh {
+            {0, 1, 2},
+            {1, 2, 3},
+            {0, 1, 4},
+            {1, 3, 5},
+            {2, 3, 6},
+            {2, 0, 7},
+    };
+
+    // Program to associate with the object
+    ppgso::Shader program = {color_vert_glsl, color_frag_glsl};
+
+    // These will hold the data and object buffers
+    GLuint vao, vbo, cbo, ibo;
+    glm::mat4 modelMatrix{1.0f};
 public:
-  // Public attributes that define position, color ..
-  glm::vec3 position{0,0,0};
-  glm::vec3 rotation{0,0,0};
-  glm::vec3 scale{1,1,1};
-  glm::vec3 color{1,0,0};
+    // Public attributes that define position, color ..
+    glm::vec3 position{0,0,0};
+    glm::vec3 rotation{0,0,0};
+    glm::vec3 scale{1,1,1};
+    glm::vec3 color{1,0,0};
 
-  // Initialize object data buffers
-  Shape() {
-    // Copy data to OpenGL
-    glGenVertexArrays(1, &vao);
-    glBindVertexArray(vao);
+    // Initialize object data buffers
+    Shape() {
+        // Copy data to OpenGL
+        glGenVertexArrays(1, &vao);
+        glBindVertexArray(vao);
 
-    // Copy positions to gpu
-    glGenBuffers(1, &vbo);
-    glBindBuffer(GL_ARRAY_BUFFER, vbo);
-    glBufferData(GL_ARRAY_BUFFER, vetrices.size() * sizeof(glm::vec3), vetrices.data(), GL_STATIC_DRAW);
+        // Copy positions to gpu
+        glGenBuffers(1, &vbo);
+        glBindBuffer(GL_ARRAY_BUFFER, vbo);
+        glBufferData(GL_ARRAY_BUFFER, vetrices.size() * sizeof(glm::vec3), vetrices.data(), GL_STATIC_DRAW);
 
-    // Set vertex program inputs
-    auto position_attrib = program.getAttribLocation("Position");
-    glEnableVertexAttribArray(position_attrib);
-    glVertexAttribPointer(position_attrib, 3, GL_FLOAT, GL_FALSE, 0, 0);
+        // Set vertex program inputs
+        auto position_attrib = program.getAttribLocation("Position");
+        glEnableVertexAttribArray(position_attrib);
+        glVertexAttribPointer(position_attrib, 3, GL_FLOAT, GL_FALSE, 0, 0);
 
-    // Copy mesh indices to gpu
-    glGenBuffers(1, &ibo);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, mesh.size() * sizeof(Face), mesh.data(), GL_STATIC_DRAW);
+        // Copy mesh indices to gpu
+        glGenBuffers(1, &ibo);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, mesh.size() * sizeof(Face), mesh.data(), GL_STATIC_DRAW);
 
-    // Set projection matrices to identity
-    program.setUniform("ProjectionMatrix", glm::mat4{1.0f});
-    program.setUniform("ViewMatrix", glm::mat4{1.0f});
-  };
-  // Clean up
-  ~Shape() {
-    // Delete data from OpenGL
-    glDeleteBuffers(1, &ibo);
-    glDeleteBuffers(1, &cbo);
-    glDeleteBuffers(1, &vbo);
-    glDeleteVertexArrays(1, &vao);
-  }
+        // Set projection matrices to identity
+        program.setUniform("ProjectionMatrix", glm::mat4{1.0f});
+        program.setUniform("ViewMatrix", glm::mat4{1.0f});
+    };
+    // Clean up
+    ~Shape() {
+        // Delete data from OpenGL
+        glDeleteBuffers(1, &ibo);
+        glDeleteBuffers(1, &cbo);
+        glDeleteBuffers(1, &vbo);
+        glDeleteVertexArrays(1, &vao);
+    }
 
-  // Set the object transformation matrix
-  void update() {
-    // TODO: Compute transformation by scaling, rotating and then translating the shape
-    // modelMatrix = ??
-  }
+    // Set the object transformation matrix
+    void update() {
+        // TODO: Compute transformation by scaling, rotating and then translating the shape
+        modelMatrix = glm::translate(glm::mat4{1.0f}, position);
+        modelMatrix = glm::rotate(modelMatrix, rotation.z, glm::vec3{0.0f, 0.0f, 1.0f});
+        modelMatrix = glm::scale(modelMatrix, scale);
+    }
 
-  // Draw polygons
-  void render(){
-    // Update transformation and color uniforms in the shader
-    program.use();
-    program.setUniform("OverallColor", color);
-    program.setUniform("ModelMatrix", modelMatrix);
+    // Draw polygons
+    void render(){
+        // Update transformation and color uniforms in the shader
+        program.use();
+        program.setUniform("OverallColor", color);
+        program.setUniform("ModelMatrix", modelMatrix);
 
-    glBindVertexArray(vao);
-    glDrawElements(GL_TRIANGLES, (GLsizei) mesh.size() * 3, GL_UNSIGNED_INT, 0);
-  };
+        glBindVertexArray(vao);
+        glDrawElements(GL_TRIANGLES, (GLsizei) mesh.size() * 3, GL_UNSIGNED_INT, 0);
+    };
 };
 
 class ShapeWindow : public ppgso::Window {
 private:
-  Shape shape1, shape2;
+    Shape shape1, shape2;
 public:
-  ShapeWindow() : Window{"task4_2dshapes", SIZE, SIZE} {
-    shape1.color = {1,0,0};
-    shape2.color = {0,1,0};
-  }
+    ShapeWindow() : Window{"task4_2dshapes", SIZE, SIZE} {
+        shape1.color = {1,0,0};
+        shape2.color = {0,1,0};
+    }
 
-  void onIdle() {
-    // Set gray background
-    glClearColor(.1f,.1f,.1f,1.0f);
-    // Clear depth and color buffers
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    void onIdle() {
+        // Set gray background
+        glClearColor(.1f,.1f,.1f,1.0f);
+        // Clear depth and color buffers
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    // Move and Render shape\    // Get time for animation
-    auto t = (float) glfwGetTime();
+        // Move and Render shape
+        // Get time for animation
+        auto t = (float) glfwGetTime();
 
-    // TODO: manipuate shape1 and shape2 position to rotate clockwise
-    //shape1.position = ??
-    //shape2.position = -shape1.position;
+        // TODO: manipuate shape1 and shape2 position to rotate clockwise
+        shape1.position = {0, 0, 0};
+        shape2.position = -shape1.position;
 
-    // Manipulate rotation of the shape
-    shape1.rotation.z = t*5.0f;
-    shape2.rotation = -shape1.rotation;
+        // Manipulate rotation of the shape
+        shape1.rotation.z = t*5.0f;
+        shape2.rotation = -shape1.rotation;
 
-    // Manipulate shape size
-    shape1.scale = {sin(t),sin(t), 1};
-    shape2.scale = -shape1.scale;
+        // Manipulate shape size
+        shape1.scale = {sin(t), sin(t), 1};
+        shape2.scale = -shape1.scale;
 
-    // Update and render each shape
-    shape1.update();
-    shape2.update();
+        // Update and render each shape
+        shape1.update();
+        shape2.update();
 
-    shape1.render();
-    shape2.render();
-  }
+        shape1.render();
+        shape2.render();
+    }
 };
 
 int main() {
-  // Create our window
-  auto window = ShapeWindow{};
+    // Create our window
+    auto window = ShapeWindow{};
 
-  // Main execution loop
-  while (window.pollEvents()) {}
+    // Main execution loop
+    while (window.pollEvents()) {}
 
-  return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
