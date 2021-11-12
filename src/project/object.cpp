079a17f0996505fbaee9e04c88e5945b613a46a8
mhrnciar <matej.hrnciar2@gmail.com>
2021-11-12 10:03:16 +0100

    Create project

---
+++ src/project/object.cpp
@@ -0,0 +1,30 @@
+#define GLM_ENABLE_EXPERIMENTAL
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtx/euler_angles.hpp>
+#include <glm/gtx/transform.hpp>
+
+#include "object.h"
+
+void Object::generateModelMatrix() {
+    glm::mat4 translationMatrix, rotationMatrix, scaleMatrix;
+
+    if(parent) { //This node has a parent...
+        translationMatrix = glm::translate(glm::mat4(1.0f), position);
+        rotationMatrix = glm::orientate4(rotation);
+        scaleMatrix = glm::scale(glm::mat4(1.0f), scale);
+        modelMatrix = translationMatrix * rotationMatrix * scaleMatrix * parent->modelMatrix;
+    }
+
+    else{ //Root node, world transform is local transform!
+        translationMatrix = glm::translate(glm::mat4(1.0f), position);
+        rotationMatrix = glm::orientate4(rotation);
+        scaleMatrix = glm::scale(glm::mat4(1.0f), scale);
+        modelMatrix = translationMatrix * rotationMatrix * scaleMatrix;
+    }
+
+    for(auto & i : children) {
+        i->generateModelMatrix();
+    }
+}
+
---
+++ src/project/rectangle.cpp
@@ -0,0 +1,63 @@
+#include "rectangle.h"
+#include "scene.h"
+
+#include <shaders/diffuse_vert_glsl.h>
+#include <shaders/diffuse_frag_glsl.h>
+
+// shared resources
+std::unique_ptr<ppgso::Mesh> Rectangle::mesh;
+std::unique_ptr<ppgso::Texture> Rectangle::texture;
+std::unique_ptr<ppgso::Shader> Rectangle::shader;
+
+Rectangle::Rectangle() {
+    // Initialize static resources if needed
+    if (!shader) shader = std::make_unique<ppgso::Shader>(diffuse_vert_glsl, diffuse_frag_glsl);
+    if (!texture) texture = std::make_unique<ppgso::Texture>(ppgso::image::loadBMP("sand.bmp"));
+    if (!mesh) mesh = std::make_unique<ppgso::Mesh>("plane.obj");
+}
+
+bool Rectangle::update(Scene &scene, float dt) {
+    // Keyboard controls
+    if(scene.keyboard[GLFW_KEY_LEFT]) {
+        position.x += 10 * dt;
+    } else if(scene.keyboard[GLFW_KEY_RIGHT]) {
+        position.x -= 10 * dt;
+    } else if(scene.keyboard[GLFW_KEY_UP]) {
+        position.y += 10 * dt;
+    } else if(scene.keyboard[GLFW_KEY_DOWN]) {
+        position.y -= 10 * dt;
+    } else {
+        rotation.z = ppgso::PI;
+        rotation.y += 2 * dt;
+    }
+
+    generateModelMatrix();
+    return true;
+}
+
+void Rectangle::render(Scene &scene) {
+    shader->use();
+
+    // Set up light
+    shader->setUniform("LightDirection", scene.lightDirection);
+
+    // use camera
+    shader->setUniform("ProjectionMatrix", scene.camera->projectionMatrix);
+    shader->setUniform("ViewMatrix", scene.camera->viewMatrix);
+
+    // render mesh
+    shader->setUniform("ModelMatrix", modelMatrix);
+    shader->setUniform("Texture", *texture);
+    mesh->render();
+
+    for(auto & i : children) {
+        i->render(scene);
+    }
+}
+
+void Rectangle::addChild(Object *s) {
+    s->parent = this;
+    children.push_back(s);
+}
+
+
